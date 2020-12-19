09d413b00279ec61ecdcad2b213196ffc12b953f
Johann Molano <jjdmolano@gmail.com>
2020-12-19 20:43:43 +0800

    Added README file on how to start the app.

---
+++ README.md
@@ -0,0 +1,11 @@
+# Node Login App made for Calibr8 Digital Exam
+
+This app was made as an exercise for my application as a junior full stack developer.
+
+The app uses the Node framework, Express to run as a basic back end login authenticator for the front end React To Do App.
+
+**Enter `nodemon index.js` in the CLI to start the app.**
+
+**Username is `User` and Password is `password`. Any other entered username or password will not work.**
+
+ If the user tries to access the page without logging in, the list will not show and will have a button to redirect them back to the login page.
--- index.js
+++ index.js
@@ -1,20 +1,18 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const users = [{ id: 'User', password: 'password' }]
+const users = [{ "id" : "User", "password": "password" }]
 
 app.use(express.json())
 app.use(cors())
 
 app.post('/login', (req, res) => {
-	const user = users.find(
-		user => user.id === req.body.id && user.password === req.body.password
-	)
+    const user = users.find(user => (user.id === req.body.id) && (user.password === req.body.password))
 	if (user == undefined) {
-		res.status(400).send({ id: 'Invalid' })
+		res.status(400).send({"id" : "Invalid"})
 	} else {
-		res.status(200).send(user)
-	}
+        res.status(200).send(user)
+    }
 })
 
 app.listen(4000, () => {
