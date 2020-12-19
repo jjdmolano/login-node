349b52c00bb309557170ebeca3bf316e522830f3
Johann Molano <jjdmolano@gmail.com>
2020-12-19 23:22:26 +0800

    Updated login to use a promise instead.

--- README.md
+++ README.md
@@ -2,9 +2,10 @@
 
 This app was made as an exercise for my application as a junior full stack developer.
 
-The app uses the Node framework, Express to run as a basic back end login authenticator for the front end React To Do App.
+The app uses the Node framework, Express to run as a basic back end login authenticator for the front end React To Do App. A promise function was used to send a response to the front end.
 
 **Enter `nodemon index.js` in the CLI to start the app.**
+
 
 **Username is `User` and Password is `password`. Any other entered username or password will not work.**
 
--- index.js
+++ index.js
@@ -1,20 +1,37 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const users = [{ "id" : "User", "password": "password" }]
+const port = 4000
+// fake/dummy user database
+const users = [{ id: 'User', password: 'password' }]
 
 app.use(express.json())
 app.use(cors())
 
+// post request for login
 app.post('/login', (req, res) => {
-    const user = users.find(user => (user.id === req.body.id) && (user.password === req.body.password))
-	if (user == undefined) {
-		res.status(400).send({"id" : "Invalid"})
-	} else {
-        res.status(200).send(user)
-    }
+	function userLogin() {
+		return new Promise((resolve, reject) => {
+			const user = users.find(
+				user => user.id === req.body.id && user.password === req.body.password
+			)
+			if (user !== undefined) {
+				resolve(res.status(200).send(user))
+			} else {
+				reject(res.status(400).send(false))
+			}
+		})
+	}
+
+	userLogin()
+		.then(success => {
+			success
+		})
+		.catch(error => {
+			error
+		})
 })
 
-app.listen(4000, () => {
-	console.log('API is now online on port 4000')
+app.listen(port, () => {
+	console.log(`API is now online on port ${port}`)
 })
