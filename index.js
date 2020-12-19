415f4c81431370b06d6ccd6e28a8c2e75e59c320
Johann Molano <jjdmolano@gmail.com>
2020-12-19 20:15:20 +0800

    Initial commit. Connected with the React Todo App for basic login functionality.

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const users = [{ id: 'User', password: 'password' }]
+
+app.use(express.json())
+app.use(cors())
+
+app.post('/login', (req, res) => {
+	const user = users.find(
+		user => user.id === req.body.id && user.password === req.body.password
+	)
+	if (user == undefined) {
+		res.status(400).send({ id: 'Invalid' })
+	} else {
+		res.status(200).send(user)
+	}
+})
+
+app.listen(4000, () => {
+	console.log('API is now online on port 4000')
+})
